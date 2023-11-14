df473248e4266f38ca1eee9c1b411c13c0debec5
Olov <olovolsson@live.se>
2023-11-14 09:49:48 +0100

    Added login menu and login manager

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp_GroupProject
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+    }
+}
