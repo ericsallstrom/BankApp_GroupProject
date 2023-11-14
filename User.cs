de32bc668ed6b15ff4ed972baf43413402061cc7
Eric Sällström <eric.sallstrom@gmail.com>
2023-11-14 13:56:11 +0100

    Publik egenskap för listan i LogInManager.

--- User.cs
+++ User.cs
@@ -8,13 +8,48 @@ namespace BankApp_GroupProject
 {
     public class User
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
+
+        private bool _verified;
 
         public User(string username, string password)
         {
-            UserName = username;
+            Username = username;
             Password = password;
+        }
+
+        public void ValidateUsername(string userInput)
+        {
+            string username = userInput.Trim();
+
+            while (!_verified)
+            {
+                if (!string.IsNullOrWhiteSpace(username) || CheckValidChar(username))
+                {
+                    Username = username;
+                    Console.WriteLine("Good");
+                    _verified = true;
+                }
+                else
+                {
+                    Console.WriteLine("Fel input! Var god ange ett korrekt användarnamn.");
+                }
+            }
+        }
+
+        private static bool CheckValidChar(string username)
+        {
+            char[] invalidChars = { ' ', '@', '$', '/', '\\', '#', '¤', '"', '!', '?' };
+
+            foreach (var c in username)
+            {
+                if (!invalidChars.Contains(c))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
