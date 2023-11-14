ed574dad4fa4e56aff3e28bc7da769ce7106f14a
Olov <olovolsson@live.se>
2023-11-14 11:45:00 +0100

    Added difference to admin or customer login.

--- User.cs
+++ User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BankApp_GroupProject
 {
-    internal class User
+    public class User
     {
         public string UserName { get; set; }
         public string Password { get; set; }
