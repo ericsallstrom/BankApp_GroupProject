82a5e3b07109ae6d3ead9ec95a647193872a4883
Eric Sällström <eric.sallstrom@gmail.com>
2023-11-15 11:59:02 +0100

    Kan nu ändra lösenord i User-klassen.

--- User.cs
+++ User.cs
@@ -8,8 +8,8 @@ namespace BankApp_GroupProject
 {
     public abstract class User
     {
-        public string Username { get; }
-        private string Password { get; }
+        public string Username { get; private set; }
+        private string Password { get; set; }
 
         private bool _verified;
 
@@ -19,15 +19,15 @@ namespace BankApp_GroupProject
             Password = password;
         }
 
-        public string VerifyNewUsername(string enteredUsername)
+        public void VerifyNewUsername(string username)
         {
             byte minLength = 4;
             byte maxLength = 24;
 
             while (!_verified)
             {
-                if (enteredUsername == null || enteredUsername.Length < minLength ||
-                    enteredUsername.Length > maxLength || CheckValidChar(enteredUsername))
+                if (username == null || username.Length < minLength ||
+                    username.Length > maxLength || CheckValidChar(username))
                 {
                     Console.Write("Ogiltigt användarnamn. Ditt användarnamn kan endast vara mellan" +
                                 "\n4-24 tecken långt och får ej innehålla några specialtecken.\n" +
@@ -37,10 +37,10 @@ namespace BankApp_GroupProject
                 }
                 else
                 {
+                    Username = username;
                     _verified = true;
                 }
             }
-            return enteredUsername;
         }
 
         public void ChangePassword()
@@ -48,10 +48,12 @@ namespace BankApp_GroupProject
             Console.Write("Skriv in ditt gamla lösenord: ");
             string oldPassword = Console.ReadLine();
 
-            if (CheckPassword(oldPassword))
+            if (oldPassword == Password)
             {
                 Console.Write("Skriv in ditt nya lösenord: ");
                 string newPassword = Console.ReadLine();
+
+                VerifyNewPassword(newPassword);                
             }
             else
             {
@@ -59,22 +61,18 @@ namespace BankApp_GroupProject
             }
         }
 
-        public bool CheckPassword(string enteredPassword)
-        {
-            return Password == enteredPassword;
-        }
-
-        public string VerifyNewPassword(string enteredPassword)
+        public void VerifyNewPassword(string password)
         {
             byte minLength = 6;
             byte maxLength = 30;
 
             while (!_verified)
             {
-                if (enteredPassword != null && enteredPassword.Length >= minLength &&
-                    enteredPassword.Length <= maxLength && enteredPassword.Any(c => char.IsAsciiLetterUpper(c))
-                    && enteredPassword.Any(c => char.IsAsciiDigit(c)) && CheckValidChar(enteredPassword))
+                if (password != null && password.Length >= minLength &&
+                    password.Length <= maxLength && password.Any(c => char.IsAsciiLetterUpper(c))
+                    && password.Any(c => char.IsAsciiDigit(c)) && CheckValidChar(password))
                 {
+                    Password = password;
                     _verified = true;
                 }
                 else
@@ -86,7 +84,11 @@ namespace BankApp_GroupProject
                     Console.Clear();
                 }
             }
-            return enteredPassword;
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
         }
 
         private static bool CheckValidChar(string input)
