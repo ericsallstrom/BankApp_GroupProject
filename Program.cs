90c9c1683c882c04ddcda3ba6549350dcea04a37
Eric Sällström <eric.sallstrom@gmail.com>
2023-11-16 09:30:48 +0100

    Fixat menyer och switch-satser.

--- Program.cs
+++ Program.cs
@@ -6,67 +6,48 @@
         static void Main(string[] args)
         {
             //Byta namn??
-            Login();
+            Run();
         }
 
         // LOGIN METHOD
-        static void Login()
+        static void Run()
         {
-            LogInManager logInManager = new LogInManager();
-            ConsoleIO io = new ConsoleIO();
+            bool runApp = true;
+            LogInManager logInManager = new();
+            ConsoleIO io = new();
 
-            // Login counter
-            int loginCounter = 0;
+            while (runApp)
+            {                
+                string userChoice = "";
 
-            while (loginCounter < 3)
-            {
-                // LÄGG TILL FELHANTERING!!!
-                Console.Clear();
-                Console.Write("Vänligen ange användarnamn: ");
-                string username = Console.ReadLine();
-                Console.Write("Vänligen ange lösenord: ");
-                string password = Console.ReadLine();
-
-                bool success = logInManager.Login(username, password);
-
-                if (success)
+                while (userChoice != "0")
                 {
-                    // Go to menu method.
-                    io.DisplayMenu();
-                    // LA TILL DENNA BREAK DÅ VI HAMNADE TILLBAKA I LOOPEN EFTER ATT VI GÅTT UR Menu() ANNARS. KOMMER SÄKERT ÄNDRAS SENARE
-                    break;
+                    ConsoleIO.DisplayMainMenu();
 
-                    if (username == "admin")
+                    userChoice = Console.ReadLine();
+
+                    switch (userChoice)
                     {
-                        // Go to admin menu method.
-                        io.DisplayAdminMenu();
-                        // LA TILL DENNA BREAK DÅ VI HAMNADE TILLBAKA I LOOPEN EFTER ATT VI GÅTT UR MENYERNA ANNARS. KOMMER SÄKERT ÄNDRAS SENARE
-                        break;
-                    }
-                    else
-                    {
-                        // Go to customer menu method.
-                        io.DisplayMenu();
-                        // LA TILL DENNA BREAK DÅ VI HAMNADE TILLBAKA I LOOPEN EFTER ATT VI GÅTT UR MENYERNA ANNARS. KOMMER SÄKERT ÄNDRAS SENARE
-                        break;
-                    }
-                }
-                else
-                {
-                    loginCounter++;
-                    if (loginCounter < 3)
-                    {
-                        Console.WriteLine("Fel användarnamn eller lösenord, försök igen...");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Du har gjort för många felaktiga försök. Programmet avslutas...");
-                        Console.ReadLine();
+                        case "1":
+                            io.LogIn();
+                            break;
+                        case "2":
+                            break;
+                        case "0":
+                            Console.Clear();
+                            Console.WriteLine("Du har valt att avsluta...\n" +
+                                    "\nVälkommen åter!");
+                            runApp = false;
+                            break;
+                        default:
+                            Console.Write("\nOgiltigt menyval! Var god välj ett alternativ från menyn." +
+                                          "\nTryck \"ENTER\" och försök igen.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
                     }
                 }
             }
-
         }
     }
 }
