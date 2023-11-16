c2adbc1f5d60b95161308f10af6d3995e31c7ffe
Olov <olovolsson@live.se>
2023-11-16 09:08:59 +0100

    Changed error message when typing wrong user/password

--- Program.cs
+++ Program.cs
@@ -49,9 +49,14 @@
                 else
                 {
                     loginCounter++;
-                    if (loginCounter < 3)
+                    if (loginCounter == 1)
                     {
-                        Console.WriteLine("Fel användarnamn eller lösenord, försök igen...");
+                        Console.WriteLine("Fel användarnamn eller lösenord, två försök kvar...");
+                        Console.ReadLine();
+                    }
+                    else if (loginCounter == 2)
+                    {
+                        Console.WriteLine("Fel användarnamn eller lösenord, ett försök kvar...");
                         Console.ReadLine();
                     }
                     else
