7661d33136ffd201d81ac05f0b49891225b856da
Bumbaclat89 <HANY89@LIVE.SE>
2023-11-16 08:50:05 +0100

    La till metoder i admin klassen

--- Admin.cs
+++ Admin.cs
@@ -5,7 +5,36 @@
         public Admin(string username, string password)
             : base(username, password)
         {
+           
 
         }
+        
+        //anropar metoden från "user" klassen för att skapa ny användare namn samt lösenord
+        public void VerifyNewUsername()
+        {
+            Console.WriteLine("Skriv ny användarnamn" + 
+                "Den kan endast vara mellan 4-24 tecken långt och får ej innehålla några specialtecken");
+        }
+
+        public void VerifyNewPassword()
+        {
+            Console.WriteLine("Skriv nytt lösenord" +
+                "tänk på det måste vara mellan 6-30 tecken långt" +
+                "Lösenordet ska innehålla minst en stor bokstav, en siffra och ett specialtecken");
+        }
+
+        public void DeleteUser()
+        {
+            //ska skapa metod i loginmanager som ta bort användare 
+        }
+
+        public void PrintUsers()
+        {
+            //inte klar än
+        }
+
+
+
+
     }
 }
