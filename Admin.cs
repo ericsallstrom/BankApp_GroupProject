c8a121d9b179e26772cd3caacc5f37b7c89aa4ff
Bumbaclat89 <HANY89@LIVE.SE>
2023-11-20 10:04:05 +0100

    La till kommentarer i admin klassen och "new user"

--- Admin.cs
+++ Admin.cs
@@ -7,29 +7,26 @@
         {
         }
         
-        //anropar metoden från "user" klassen för att skapa ny användare namn samt lösenord
-        public void VerifyNewUsername()
-        {
-            Console.WriteLine("Skriv ny användarnamn" + 
-                "Den kan endast vara mellan 4-24 tecken långt och får ej innehålla några specialtecken");
-        }
+        ////anropar metoden från "user" klassen för att skapa ny användare namn samt lösenord
+        //public void VerifyNewUsername()
+        //{
+        //    Console.WriteLine("Skriv ny användarnamn" + 
+        //        "Den kan endast vara mellan 4-24 tecken långt och får ej innehålla några specialtecken");
+        //}
 
-        public void VerifyNewPassword()
-        {
-            Console.WriteLine("Skriv nytt lösenord" +
-                "tänk på det måste vara mellan 6-30 tecken långt" +
-                "Lösenordet ska innehålla minst en stor bokstav, en siffra och ett specialtecken");
-        }
+        //public void VerifyNewPassword()
+        //{
+        //    Console.WriteLine("Skriv nytt lösenord" +
+        //        "tänk på det måste vara mellan 6-30 tecken långt" +
+        //        "Lösenordet ska innehålla minst en stor bokstav, en siffra och ett specialtecken");
+        //}
 
         public void DeleteUser()
         {
             Console.WriteLine("Användaren är borttagen");
         }
 
-        public void PrintUsers()
-        {
-            //Finns i LoginManager klassen, Vet ej om vi behöver den här
-        }
+        
 
 
 
